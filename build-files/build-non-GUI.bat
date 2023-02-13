73f4b2e065db4f2e301f6956f32e3072b93ac24c
xxcosita3czxx <kokosove18@gmail.com>
2023-02-13 13:00:01 +0100

    Update build-non-GUI.bat

--- build-files/build-non-GUI.bat
+++ build-files/build-non-GUI.bat
@@ -2,4 +2,4 @@
 pip install pyinstaller
 pip install click
 
-pyinstaller --onefile --add-data=../non-GUI/mcpcacconfig.py;mcpcacconfig.py ../non-GUI/mc-pc-ac.py
+python -m pyinstaller --onefile --add-data=../non-GUI/mcpcacconfig.py;mcpcacconfig.py ../non-GUI/mc-pc-ac.py
